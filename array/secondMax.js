f3ff044c64eb26bcb8a6c6f00fe2c5e257988548
Aakash Patel <akashpatel8818@gmail.com>
2025-07-03 22:05:37 +0530

    SecondMax fixing bugs

---
+++ array/secondMax.js
@@ -0,0 +1,14 @@
+let arr = [10, 30, 666, 43, 53, 54, 56]
+
+let max = Math.max(arr[0] , arr[1])
+let sMin = Math.min(arr[0], arr[1])
+
+for(let i = 2; i < arr.length ; i++){
+    if(arr[i] > max){
+        
+    }
+}
+
+
+// console.log(sMin)
+console.log(max)
