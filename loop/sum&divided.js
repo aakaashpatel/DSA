314a84f9a8046d1725b7cdbfd90bd368a2c78562
Aakash Patel <akashpatel8818@gmail.com>
2025-07-11 07:43:12 +0530

    Sum&multi-done

--- loop/sum&divided.js
+++ loop/sum&divided.js
@@ -1,15 +1,22 @@
 const number = Number(prompt("Enter your number"));
 
-if(isNaN(number)){
-    console.log("invalid input")
-}else if(number < 0){
-    console.log("number should be +ve")
-}else{
-     let sum = 0;
-     for (let i = 0; i <= number ; i++){
-        sum = sum + i;
-     }
-     console.log(sum)
+if (number === null) {
+  console.log(cancel);
+}
+
+if (isNaN(number)) {
+  console.log("invalid input");
+} else if (number < 0) {
+  console.log("number should be +ve");
+} else {
+  let sum = 0;
+  let divi = 1;
+  for (let i = 1; i <= number; i++) {
+    sum += i;
+    divi *= i;
+  }
+  console.log(sum);
+  console.log(divi);
 }
 
 // console.log(number)
