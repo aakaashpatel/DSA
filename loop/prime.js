8179d591fa2636eee7ff7c2d2b6c072cce0aeeda
Aakash Patel <akashpatel8818@gmail.com>
2025-07-17 22:42:14 +0530

    prime-done

--- loop/prime.js
+++ loop/prime.js
@@ -1,14 +1,15 @@
-let a = 1 ;
+let a = 39;
 let isPrime = true;
 
-if( a <= 1){
-    isPrime = false ; 
-}else{
-    for(let i=2; i <=  Math.floor(a/2); i++){
-        if(a%i===0){
-            console.log("prime hain")
-        }else{
-            console.log("prime nahi h ")
+if (a <= 1) {
+    isPrime = false;
+} else {
+    for (let i = 2; i <= Math.sqrt(a); i++) {
+        if (a % i === 0) {
+            isPrime = false;
+            break; 
         }
     }
 }
+
+console.log(a + (isPrime ? " prime hai" : " prime nahi hai"));
