2964466822f30d4b8022a6c9a4e520af91c649bd
Aakash Patel <akashpatel8818@gmail.com>
2025-07-18 22:27:46 +0530

    while-learning

--- loop/while.js
+++ loop/while.js
@@ -1,1 +1,11 @@
 // jab tak answer na aa jaye tab tak while loop chalta rhe 
+
+let pr = prompt("Enter you number")
+
+if(isNaN(pr)){
+    console.log("please enter the number")
+}else{
+    if(pr>0){
+        let sum = 0;
+    }
+}
