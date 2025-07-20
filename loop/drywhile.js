4515c69e5c780a87cb09cfb1dcd9b32bfa639cf9
Aakash Patel <akashpatel8818@gmail.com>
2025-07-20 20:31:56 +0530

    drywhile

--- loop/drywhile.js
+++ loop/drywhile.js
@@ -1,13 +1,19 @@
-let pr = prompt("Enter you number ");
+let pr = prompt("Enter your number");
 
-let n = Number(pr)
+let n = Number(pr);
 
-if(isNaN(n)){
+if (isNaN(n)) {
   console.log("Enter Number");
-}else{
-  if(n > 0){
-    console.log("you number is valid and bigger than 0") //true
-  }else{
-    console.log("smaller than 0") //false
+} else {
+  if (n > 0) {
+    let sum = 0;
+    while (n > 0) {
+      let digit = n % 10;    // आखिरी digit निकालना
+      sum = sum + digit;     // sum में जोड़ना
+      n = Math.floor(n / 10); // आखिरी digit हटाना
+    }
+    console.log("Sum of digits:", sum);
+  } else {
+    console.log("smaller than 0");
   }
 }
