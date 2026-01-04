3da3fdd4689d5d235119790c5c5c7cd27c42d6f4
Aakash Patel <akashpatel8818@gmail.com>
2026-01-04 23:30:06 +0530

    sample qestion

--- index.js
+++
@@ -1,5 +0,0 @@
-const number = parseInt(prompt("Enter the number"));
-
-for (let i = 1; i <= number; i++) {
-    console.log(i);
-}
---
+++ sample/index.js
@@ -0,0 +1,7 @@
+function handleSubmit() {
+  const number = document.getElementById("prompt").value;
+  // console.log(number)
+  for (i = 1; i <= number; i++) {
+    console.log(i);
+  }
+}
