bfca83d00d6870eaf5cf88aff5d4da8f44e434c8
Aakash Patel <akashpatel8818@gmail.com>
2026-01-05 20:35:07 +0530

    how many number even and odd

--- even/index.js
+++ even/index.js
@@ -6,3 +6,15 @@ const handleSubmit = () => {
         console.log("odd")
     }
 };
+
+// const handleSubmit = () =>{
+//     const number = document.getElementById("prompt").value;
+
+//     for(let i = 0; i<=number; i++){
+//         if(i % 2 === 0 ){
+//             console.log( i ,"even")
+//         }else{
+//             console.log("odd")
+//         }
+//     }
+// }
