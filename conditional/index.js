78e2e0fd8040a199b57ea09c205ac3dbf688df44
Aakash Patel <akashpatel8818@gmail.com>
2026-01-06 23:02:12 +0530

    check the postive and negative

--- conditional/index.js
+++ conditional/index.js
@@ -1,13 +1,16 @@
 function maxofthree(a, b, c) {
-  if (a > b && a > c) {
+    let max;
+//   debugger;
+  if (a >= b && a >= c) {
     // 14 > b && 14 > 99  == false
-    console.log("max number :", a);
-  } else if (b > c && b > a) {
+    console.log("max number :", {a});
+  } else if (  b >= {a}) {
     //  6 > 99 && 6 > 14 == false
-    console.log("max number :", b);
+    console.log("max number :", {b});
   } else {
     // 99 > 14 && 99 > 6
-    console.log("max number :", c);
+    console.log("max number :", {c});
   }
+  return max
 }
-maxofthree(14, 6, 99);
+maxofthree(99, 99, 2);
---
+++ conditional/positive.js
@@ -0,0 +1,12 @@
+function zero(){
+   let number = 9;
+//    let zero ;
+
+   if( number >= 0){
+    //0 >= 9  false 
+    console.log("positive :", number)
+   }else{
+    console.log("negative :", number)
+   }
+}
+zero()
