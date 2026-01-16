8499bb54a22ab0e81f3f1314b271a8ef0c711636
Aakash Patel <akashpatel8818@gmail.com>
2026-01-16 22:39:46 +0530

    revision the average number

--- Practice/index.js
+++ Practice/index.js
@@ -1,11 +1,19 @@
-array = [53, 56, 6, 7,8 ,4, 3];
+array = [53, 56, 6, 7, 8, 4, 3, 6, 7, 8, 4];
+
+// average 
+let sum = 0
+for(let i = 0 ; i < array.length; i++){
+   sum += array[i]
+}
+console.log(sum/array.length)
+
 
 // even
 let bag = [];
-for(let i = 0 ; i < array.length; i++){
-   if(array[i] % 2 == 0){
-     bag.push(array[i])
-   }
-//    console.log(bag)
+for (let i = 0; i < array.length; i++) {
+  if (array[i] % 2 == 0) {
+    bag.push(array[i]);
+  }
+  //    console.log(bag)
 }
-console.log(bag)
+console.log("even", bag);
