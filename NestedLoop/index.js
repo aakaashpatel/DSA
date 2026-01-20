14f50ea17d88e1ec1466c8527fed10170b6dacf6
Aakash Patel <akashpatel8818@gmail.com>
2026-01-20 22:29:47 +0530

    Dry of nested loop

--- NestedLoop/index.js
+++ NestedLoop/index.js
@@ -8,3 +8,23 @@ for (let i = 1; i <= 100; i = i + 10) {
 
 
 // do more run dry 
+for (let i = 1; i <= 100; i = i + 10) {
+      for (let j = i ; j >= i + 10; i++){
+        console.log(j)
+      }
+}
+
+
+
+
+// do more run dry 
+for (let i = 1; i <= 100; i = i + 10) {
+      for (let j = i ; j >= i + 10; i++){
+        console.log(j)
+      }
+}
+
+
+
+
+// do more run dry 
